cb23f0858a7732f4aadeab62e33b8b84dea0f930
Martinho23 <ajoni844@gmail.com>
2018-04-23 13:20:38 +0200

    fixed return values of wstr and putchar functions

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: jfarinha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 11:13:59 by jfarinha          #+#    #+#             */
-/*   Updated: 2018/04/18 07:32:17 by jfarinha         ###   ########.fr       */
+/*   Updated: 2018/04/23 13:17:10 by jfarinha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,28 @@ static void	putchar_fd(int c, int fd)
 
 int		ft_putchar_fd(int c, int fd)
 {
-	if (fd < 0)
-		return (-1);
-	if (c <= 0x7F)
+	if (c <= 0x7F && fd >= 0)
 		putchar_fd(c, fd);
-	else if (c <= 0x7FF)
+	else if (c <= 0x7FF && fd >= 0)
 	{
 		putchar_fd((c >> 6) + 0xC0, fd);
 		putchar_fd((c & 0x3F) + 0x80, fd);
+		return (2);
 	}
-	else if (c <= 0xFFFF)
+	else if (c <= 0xFFFF && fd >= 0)
 	{
 		putchar_fd((c >> 12) + 0xE0, fd);
 		putchar_fd(((c >> 6) & 0x3F) + 0x80, fd);
 		putchar_fd((c & 0x3F) + 0x80, fd);
+		return (3);
 	}
-	else if (c <= 0x10FFFF)
+	else if (c <= 0x10FFFF && fd >= 0)
 	{
 		putchar_fd((c >> 18) + 0xF0, fd);
 		putchar_fd(((c >> 12) & 0x3F) + 0x80, fd);
 		putchar_fd(((c >> 6) & 0x3F) + 0x80, fd);
 		putchar_fd((c & 0x3F) + 0x80, fd);
+		return (4);
 	}
-	return (1);
+	return ((fd >= 0) ? (1) : (-1));
 }
